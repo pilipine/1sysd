0dad5749b1ce91687aa2db82ec25ace56ed05ba9
philippine <philippine@debian.hexa.local>
2026-02-07 18:07:11 +0100

    ceux ci est un test

---
+++ test/test2.c
@@ -0,0 +1,14 @@
+//.déclaration et manipulation des variables
+
+
+#include <stdio.h>  // Pour printf
+
+int main(void) {
+    int x = 42, y;
+    y = 12;
+
+    printf("Hello World!\n");
+    printf("%d %d\n", x, x + y);
+
+    return 0;   // Doit être DANS la fonction main
+}
