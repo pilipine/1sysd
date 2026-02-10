dfec72335719628406297849735e5363562ca335
philippine <philippine@debian.hexa.local>
2026-02-10 11:55:31 +0100

    pas fini

--- TP03/calculatrice.c
+++ TP03/calculatrice.c
@@ -6,10 +6,10 @@ int main() {
 	double first_number,second_number, result;
 
 	printf("Donner un premier nombre : ");
-	scanf("%f", &first_number);
+	scanf("%lf", &first_number);
 
 	printf("Donner un deuxième nombre : ");
-	scanf ("%f", &second_number);
+	scanf ("%lf", &second_number);
 
 	printf("\n 1.+(addition)\n 2.-(soustraction)\n 3.*(multiplication)\n 4./(division)\n");
 	scanf("%d", &op);
@@ -17,15 +17,19 @@ int main() {
 	switch (op) {
 		case 1:
 		result = first_number + second_number;
+		printf("%f", result);
 		break;
 		case 2:
                 result = first_number - second_number;
+		printf("%f", result);
 		break;
  		case 3:
                 result = first_number * second_number;
+		printf("%f", result);
 		break;
                 case 4:
                 result = first_number / second_number;
+		printf("%f", result);
 		break;
 		default:
 		printf("Mauvais saisie de l'opération");
