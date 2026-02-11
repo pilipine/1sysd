024cec2866c10834e15a24b020ffa20997850f78
philippine <philippine@debian.hexa.local>
2026-02-11 10:12:28 +0100

    fini

---
+++ Exemple/asks.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+
+    printf("Entrez un nombre : ");
+    scanf("%d", &n);
+    printf("Plus 42 : %d\n", n + 42);
+}
--- TP04/test.c
+++ TP04/test.c
@@ -3,7 +3,17 @@
 #include<stdio.h>
 
 int main() {
-int valeur , data[5];
+	int valeur , data[5];
+	int somme = 0;
 
-	printf("Donner un nombre :");
-	scanf("%d", &valeur); }
+	printf("Donner un nombre (max 5): ");
+	scanf("%d", &valeur); 
+
+	for (int i = 0; i < valeur; i++){
+		printf("Entrer un nombre : ");
+		scanf("%d", &data[i]);
+		somme+= data[i];
+	}
+	printf("La somme de tous ces nombres est %d\n",somme);
+
+}
