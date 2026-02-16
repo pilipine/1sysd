643a8e91c7fd193979aca239fe62bfe5486b7c93
philippine <philippine@debian.hexa.local>
2026-02-16 11:31:40 +0100

    fini

---
+++ TP07/sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+#define MAX 100
+
+int main(int argc, char *argv[]) {
+    double numbers[MAX];
+    int i, sum;
+
+    for( i = 1; i < argc; i++) {
+	val = strtod(argv[i]NULL);
+	sum += val;
+
+	}
+
+	printf("%.2lf\n", sim);
+	return 0;
+}
+"sum.c" 17L. 291B
